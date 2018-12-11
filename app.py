a7ca8ebdd8b628479c3ceb7b47ebe6cab9762965
xeranimus <nobodycaresaboutspam@gmail.com>
2018-12-11 17:33:30 -0500

    getting there

--- app.py
+++ app.py
@@ -1,69 +1,108 @@
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-import sys
+from PyQt5.QtCore import Qt 
+from PyQt5.QtGui import * 
 
-qApp = QApplication([])
+def set_dark_style(q_app):
+    q_app.setStyle("Fusion")
+    dark_palette = QPalette()
 
-qApp.setStyle("Fusion")
+    dark_palette.setColor(QPalette.Window, QColor(53, 53, 53))
+    dark_palette.setColor(QPalette.WindowText, Qt.white)
+    dark_palette.setColor(QPalette.Base, QColor(25, 25, 25))
+    dark_palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+    dark_palette.setColor(QPalette.ToolTipBase, Qt.white)
+    dark_palette.setColor(QPalette.ToolTipText, Qt.white)
+    dark_palette.setColor(QPalette.Text, Qt.white)
+    dark_palette.setColor(QPalette.Button, QColor(53, 53, 53))
+    dark_palette.setColor(QPalette.ButtonText, Qt.white)
+    dark_palette.setColor(QPalette.BrightText, Qt.red)
+    dark_palette.setColor(QPalette.Link, QColor(42, 130, 218))
+    dark_palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+    dark_palette.setColor(QPalette.HighlightedText, Qt.black)
 
-dark_palette = QPalette()
+    qApp.setPalette(dark_palette)
+    
 
-dark_palette.setColor(QPalette.Window, QColor(53, 53, 53))
-dark_palette.setColor(QPalette.WindowText, Qt.white)
-dark_palette.setColor(QPalette.Base, QColor(25, 25, 25))
-dark_palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
-dark_palette.setColor(QPalette.ToolTipBase, Qt.white)
-dark_palette.setColor(QPalette.ToolTipText, Qt.white)
-dark_palette.setColor(QPalette.Text, Qt.white)
-dark_palette.setColor(QPalette.Button, QColor(53, 53, 53))
-dark_palette.setColor(QPalette.ButtonText, Qt.white)
-dark_palette.setColor(QPalette.BrightText, Qt.red)
-dark_palette.setColor(QPalette.Link, QColor(42, 130, 218))
-dark_palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
-dark_palette.setColor(QPalette.HighlightedText, Qt.black)
+        #do something
+        #do_loop = False
 
-qApp.setPalette(dark_palette)
-
-
-def on_button_clicked_c():
-    alert = QMessageBox()
-    alert.setText('Create a new data entry')
-    alert.exec_()
-
-def on_button_clicked_u():
-    alert = QMessageBox()
-    alert.setText('Update a data entry')
-    alert.exec_()
+class SubWindow(QMainWindow):
+    def __init__(self, parent):
+        super().__init__(parent)
 
 
-window = QWidget()
-buttons = QWidget()
-layout = QHBoxLayout()
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        container = QWidget()
+        buttons = QWidget()
+        layout = QHBoxLayout()
 
-c_button = QPushButton('New')
-u_button = QPushButton('Update')
+        c_button = QPushButton('New')
+        u_button = QPushButton('Update')
 
-c_button.setStyleSheet('padding: 5px')
+        c_button.setStyleSheet('padding: 5px')
 
-c_button.clicked.connect(on_button_clicked_c)
-u_button.clicked.connect(on_button_clicked_u)
+        c_button.clicked.connect(self.on_button_clicked_c)
+        u_button.clicked.connect(self.on_button_clicked_u)
 
-layout.addWidget(c_button)
-layout.addWidget(u_button)
+        label = QLabel("Search")
+        query = QLineEdit()
+        layout.addWidget(label)
+        layout.addWidget(query)
+        layout.addWidget(c_button)
+        layout.addWidget(u_button)
+        #=====
+        self.dialog_create = SubWindow(self)
+        self.dialog_update = SubWindow(self)
+        container_create = QWidget()
+        form = QHBoxLayout()
 
-f_layout = QFormLayout()
-label = QLabel("Search")
-query = QLineEdit()
+        label = QLabel('Create New ')
 
-f_layout.addRow(label, query)
+        input1 = QComboBox()
+        input1.addItem('Flowers')
+        input1.addItem('Sightings')
+        input1.addItem('Features')
 
-buttons.setLayout(layout)
-f_layout.addWidget(buttons)
-window.setLayout(f_layout)
+        input1.currentTextChanged.connect(self.on_combobox_changed)
 
-window.show()
+        ok_button = QPushButton('ok')
+
+        form.addWidget(label)
+        form.addWidget(input1)    
+        container_create.setLayout(form)
+        self.dialog_create.setCentralWidget(container_create)
+        #=====
+        f_layout = QVBoxLayout()
 
 
-qApp.setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }")
-qApp.exec_()
+        buttons.setLayout(layout)
+
+        results_container = QWidget()
+        results_container.setStyleSheet('padding: 200px')
+        results_label = QLabel('Results:')
+
+        f_layout.addWidget(buttons)
+        f_layout.addWidget(results_label)
+        container.setLayout(f_layout)
+
+        self.setCentralWidget(container)
+        self.show()
+
+    def on_button_clicked_c(self):
+        self.dialog_create.show()
+    
+    def on_button_clicked_u(self):
+        self.dialog_update.show()
+    def on_combobox_changed(self, value):
+        print("Value: ", value)
+
+        
+
+if __name__ == "__main__":
+    qApp = QApplication([])
+    set_dark_style(qApp)
+    window = MainWindow()
+    qApp.setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }")
+    qApp.exec_()
